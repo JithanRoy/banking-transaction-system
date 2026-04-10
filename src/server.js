b7d44882c5afd0460e7cf0530e9e63cda8fb72ad
JithanRoy <jithan.roy@tecnonext.com>
2026-04-10 21:23:06 +0600

    feat: update CORS configuration to allow PUT and DELETE methods

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
 
